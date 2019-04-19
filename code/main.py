50927baf16c5e4c2c0d56a823d316ff17c98f679
Bessie Jiang <bjiang@bessies-mbp-4.devices.brown.edu>
2019-04-18 20:33:08 -0400

    Added gitignore, flags to choose which image, H@B image folder

--- code/main.py
+++ code/main.py
@@ -1,33 +1,19 @@
 import numpy as np
 import os
+import sys
 from skimage import io, color
 from remove import median_filter
 
-def main():
+def main(folder):
     data_dir = os.path.dirname(__file__) + '../data/'
-    # amsterdam_folder = os.path.join(data_dir, "ams")
-    # amsterdam_images = []
-    # for f in os.listdir(amsterdam_folder):
-    #     img = io.imread(os.path.join(amsterdam_folder, f))
-    #     if img is not None:
-    #         amsterdam_images.append(img)
-    # io.imsave('amsterdam.png', median_filter(amsterdam_images))
-
-    # pond_folder = os.path.join(data_dir, "pond")
-    # pond_images = []
-    # for f in os.listdir(pond_folder):
-    #     img = io.imread(os.path.join(pond_folder, f))
-    #     if img is not None:
-    #         pond_images.append(img)
-    # io.imsave('pond.png', median_filter(pond_images))
-
-    lecture_folder = os.path.join(data_dir, "lecture-capture")
-    lecture_images = []
-    for f in os.listdir(lecture_folder):
-        img = io.imread(os.path.join(lecture_folder, f))
+    img_folder = os.path.join(data_dir, folder)
+    images = []
+    for f in os.listdir(img_folder):
+        img = io.imread(os.path.join(img_folder, f))
         if img is not None:
-            lecture_images.append(img)
-    io.imsave('lecture.png', median_filter(lecture_images))
+            images.append(img)
+    io.imsave(folder + '.png', median_filter(images))
 
 if __name__ == '__main__':
-    main()
+    folder = 'ams' if len(sys.argv) == 1 else sys.argv[1]
+    main(folder)
