846ffe87ee966636e819b65e670633a1fecbfbd1
Lucy <lucyhyw@gmail.com>
2019-04-23 19:34:30 -0400

    Add images for the main green. Add functionality to create masks

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 from skimage import io, color
-from remove import median_filter
+from remove import median_filter, subtract
 
 def main():
     data_dir = os.path.dirname(__file__) + '../data/'
@@ -21,13 +21,33 @@ def main():
     #         pond_images.append(img)
     # io.imsave('pond.png', median_filter(pond_images))
 
-    lecture_folder = os.path.join(data_dir, "lecture-capture")
-    lecture_images = []
-    for f in os.listdir(lecture_folder):
-        img = io.imread(os.path.join(lecture_folder, f))
-        if img is not None:
-            lecture_images.append(img)
-    io.imsave('lecture.png', median_filter(lecture_images))
+    # lecture_folder = os.path.join(data_dir, "lecture-capture")
+    # lecture_images = []
+    # for f in os.listdir(lecture_folder):
+    #     img = io.imread(os.path.join(lecture_folder, f))
+    #     if img is not None:
+    #         lecture_images.append(img)
+    # io.imsave('lecture.png', median_filter(lecture_images))
+
+    green_folder = os.path.join(data_dir, "green")
+    # green_images = []
+    # num_images = len(os.listdir(green_folder))
+    # if (num_images % 2 == 0): # if we have an even number of images
+    #     last_img = os.listdir(green_folder)[num_images - 1] # pick last image
+    #     file_last_img = os.path.join(green_folder, last_img)
+    #     print(file_last_img)
+    #     os.system("cp " + file_last_img + " " + os.path.join(green_folder, "duplicate.jpg"))
+    # for f in os.listdir(green_folder):
+    #     img = io.imread(os.path.join(green_folder, f))
+    #     if img is not None:
+    #         green_images.append(img)
+    # io.imsave('green.png', median_filter(green_images))
+    green_folder = os.path.join(data_dir, "green")
+    green_saved = "green.png"
+    img0 = io.imread(os.path.join(green_folder, "009.jpg"))
+    img1 = io.imread(os.path.join(green_folder, "009.jpg"), as_gray=True)
+    img2 = io.imread(green_saved, as_gray=True)
+    io.imsave('hahaha.png', subtract(img0,img1,img2))
 
 if __name__ == '__main__':
     main()
--- code/remove.py
+++ code/remove.py
@@ -11,8 +11,18 @@ def median_filter(images):
         channel = img_arr[:,:,:,i]
         print(channel)
         res[:,:,i] = signal.medfilt(channel, kernel_size=(n,1,1))[n//2]
-    print("res")
-    print(res)
+    # print("res")
+    # print(res)
+    return np.array(res).astype(int)
+
+def subtract(img0, img1, img2):
+    w,h,c = img0.shape
+    res = np.zeros((w,h, c))
+    channel1 = img1
+    channel2 = img2
+    greyscaleMask = np.array(np.clip(channel1 - channel2, 0, 1))#.astype(int)
+    for i in range(c):
+        res[:,:,i] = img0[:,:,i] * greyscaleMask + 10
     return np.array(res).astype(int)
 
 if __name__ == '__main__':
