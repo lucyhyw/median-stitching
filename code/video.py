50a7036063683204070a51ea16838502eb709a82
Bessie Jiang <bjiang@bessies-mbp-4.devices.brown.edu>
2019-05-01 14:41:14 -0400

    Modified the video.py file so it gets every 15th frame for the first minute and writes it to a video

--- code/video.py
+++ code/video.py
@@ -1,29 +1,27 @@
 import numpy as np
 import cv2
+from remove import median_filter
 
-cap = cv2.VideoCapture('../data/green.mov')
+cap = cv2.VideoCapture('./green.mov')
 allFrames = []
-# frames = np.arange(0, 10800, 15)
 frames = np.arange(0, 2250, 15)
-frames = np.reshape(frames, (-1, 10))
-frames = np.transpose(frames)
-# print(frames)
-# count = 0
-while(cap.isOpened()):
+ret = True
+
+while(cap.isOpened() and ret):
     ret, frame = cap.read()
-    allFrames.append(frame)
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if ret:
+        allFrames.append(np.array(frame))
+
 allFrames = np.array(allFrames)
-print(allFrames.shape)
-framesWeWant = allFrames[frames]
-print(framesWeWant)
+framesWeWant = np.array([allFrames[i] for i in frames])
 
-# print(everyFiveFrames)
+height, width, layers = allFrames[0].shape
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter('green_video.avi',fourcc,2,(width,height))
+for frame in framesWeWant:
+    video.write(frame)
 
 cap.release()
+video.release()
 cv2.destroyAllWindows()
