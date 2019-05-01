7c2586839048b942b4b76af1c907978de9e6db0a
Bessie Jiang <bjiang@bessies-mbp-4.devices.brown.edu>
2019-05-01 16:18:41 -0400

    Added video demo, modified code so it median filters the frames in a given window

--- code/video.py
+++ code/video.py
@@ -4,8 +4,9 @@ from remove import median_filter
 
 cap = cv2.VideoCapture('./green.mov')
 allFrames = []
-frames = np.arange(0, 2250, 15)
+frames = np.arange(0, 10700, 15)
 ret = True
+WINDOW_SIZE = 40
 
 while(cap.isOpened() and ret):
     ret, frame = cap.read()
@@ -18,9 +19,14 @@ framesWeWant = np.array([allFrames[i] for i in frames])
 
 height, width, layers = allFrames[0].shape
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-video = cv2.VideoWriter('green_video.avi',fourcc,2,(width,height))
-for frame in framesWeWant:
-    video.write(frame)
+video = cv2.VideoWriter('green_video.avi',fourcc,8,(width,height))
+for i in range(len(framesWeWant)):
+    if (i < WINDOW_SIZE):
+        video.write(np.uint8(median_filter(framesWeWant[i: i + WINDOW_SIZE+1: 10,:,:,:])))
+    elif (i >= len(framesWeWant) - WINDOW_SIZE):
+        video.write(np.uint8(median_filter(framesWeWant[i-WINDOW_SIZE: i+1: 10,:,:,:])))
+    else:
+        video.write(np.uint8(median_filter(framesWeWant[i-WINDOW_SIZE: i+WINDOW_SIZE + 1,:,:,:])))
 
 cap.release()
 video.release()
